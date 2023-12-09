eb8ca05c129524a186b4bc9cd0c5c29387626b3a
Martin-Mercado <mercadomartin364@gmail.com>
2023-12-08 23:41:01 -0300

    socket, binding & listening

--- Networking/Server.c
+++ Networking/Server.c
@@ -1,5 +1,9 @@
 #include  "Server.h"
 
+#include  <stdio.h>
+#include <stdlib.h>
+
+
 struct Server server_constructor(int domain, int service, int protocol, unsigned long interface, int port, int backlog, void (*launch)(void))
 {
     struct Server server;
@@ -22,6 +26,30 @@ struct Server server_constructor(int domain, int service, int protocol, unsigned
     //htonl, mismo que htons pero para long
 
     server.socket = socket(domain, service, protocol);
+
+
+    //test de socket
+    if(server.socket == 0)
+    {
+        perror("no se pudo conectar al socket\n");
+        exit(1);
+    }
+    
+    //realizar el binding
+    if(bind(server.socket, (struct sockaddr *)&server.address, sizeof(server.address)) < 0)
+    {
+        perror("No se pudo hacer el binding\n");
+        exit(1);
+    }
+
+    if(listen(server.socket, server.backlog) < 0)
+    {
+        perror("Error al empezar el listening\n");
+        exit(1);
+    }
+
+    //funcion de inicializacion
+    server.launch = launch;
 
     return server;
 }
