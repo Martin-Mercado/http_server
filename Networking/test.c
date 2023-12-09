4ce365e13e9c87fdefa6010ac9e7dde884aaeadb
Martin-Mercado <mercadomartin364@gmail.com>
2023-12-09 15:56:20 -0300

    undefined reference to `server_constructor'?????

--- Networking/test.c
+++ Networking/test.c
@@ -6,28 +6,36 @@
 //definir funcion de inicializacion
 void launch(struct Server *server)
 {
-    printf("==== ESPERANDO CONEXION ====");
-
+    
     // Definir el buffer
     char buffer[30000];
 
-    int address_len = sizeof(server->address);
+    while(1)
+    {
+        printf("in while\n");
+        printf("==== ESPERANDO CONEXION ====\n");
 
-    // Declarar nuevo socket
-    int new_socket = accept(server->socket,(struct sockaddr *)&server->address, (socklen_t *)&address_len);
+        int address_len = sizeof(server->address);
 
-    read(new_socket,buffer,30000);
+        // Declarar nuevo socket
+        int new_socket = accept(server->socket,(struct sockaddr *)&server->address, (socklen_t *)&address_len);
 
-    printf("%s\n", buffer);
+        read(new_socket,buffer,30000);
 
-    char *hello = "HTTP/1.1 200 OK\nDate: Mon, 27 Jul 2009 12:28:53 GMT\nServer: Apache/2.2.14 (Win32)\nLast-Modified: Wed, 22,Jul 2009 19:15:56 GMT\nContent-Length: 88\nContent-Type: text/html\nConnection: Closed\n<html><body><h1>Homo Deus</h1></body></html>";
-    write(new_socket, hello,strlen(hello));
+        printf("%s\n", buffer);
 
-    close(new_socket);
+        char *hello = "HTTP/1.1 200 OK\nDate: Mon, 27 Jul 2009 12:28:53 GMT\nServer: Apache/2.2.14 (Win32)\nLast-Modified: Wed, 22,Jul 2009 19:15:56 GMT\nContent-Length: 88\nContent-Type: text/html\nConnection: Closed\n<html><body><h1>Homo Deus</h1></body></html>";
+        write(new_socket, hello,strlen(hello));
+
+        close(new_socket);
+    }
+    
 }
 
 int main()
 {
+    printf("Entered Main\n");
     struct Server server = server_constructor(AF_INET, SOCK_STREAM, 0, INADDR_ANY, 80,10, launch);
+    printf("Saliendo de constructor\n");
     server.launch(&server);
 }
